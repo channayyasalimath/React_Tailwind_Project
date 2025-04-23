d8b4fbc92e20a76f11bec9d4a162aadef7274f24
channayyasalimath <salimathchannayya@gmail.com>
2025-04-23 19:05:54 +0530

    developed and added Hero Section to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 
 function App() {
   
 
   return (
-    <>
+    <main className='relative min-h-screen overflow-x-hidden'>
+      <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10'></div>
+      <div className='overflow-hidden'>
       <NavBar />
-    </>
+      <Hero />
+      </div>
+    </main>
   )
 }
 
